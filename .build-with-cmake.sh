80f73520f4048a048186cec7fee8253ce16713f9
Geirmundur Orri Sigurdsson <orri@icesoft.is>
2018-09-28 15:17:28 +0200

    Update the build information script

--- .build-with-cmake.sh
+++ .build-with-cmake.sh
@@ -1,11 +1,29 @@
 #!/bin/sh
 
+echo ""
+echo "================================================================================"
+echo "Building the Envelope Protection Solution on Linux/Unix"
+echo "================================================================================"
+echo ""
+
+echo "--------------------------------------------------------------------------------"
+#echo "Building with the auto-tools from the root directory"
+echo "Building with cmake from the root directory"
+echo "--------------------------------------------------------------------------------"
+
+# Auto-tools build
+#autoreconf -ivf
+#./configure
+#make
+#make check
+
 # Create the build (if on amazon use cmake3)
+echo "Create the build with cmake"
 cmake .
 
-# Build the projec
+echo "Build the project"
 make
 
-# Execute the test
-./cpp/tests/assumption_tests
+echo "Execute the thest"
+./cpp/tests/assumption_tests > assumption_tests.log
 
